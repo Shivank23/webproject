bc71101e9f1a393f74700226ec02f2476db29b0c
Shivank23 <shivankpandey23@gmail.com>
2020-12-07 15:06:21 +0700

    nodejs project

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs')
+const port = process.env.PORT || 3000;
+const path = require("path")
+//public static path
+const static_path = path.join(__dirname, "../public")
+const template_path = path.join(__dirname, "../templates/views")
+const partials_path = path.join(__dirname, "../templates/partials")
+app.set('view engine', 'hbs')
+app.set('views', template_path)
+hbs.registerPartials(partials_path)
+
+app.use(express.static(static_path));
+
+app.get("", (req, res) => {
+    res.render("index", null)
+})
+app.get("/about", (req, res) => {
+    res.render("about", null)
+})
+app.get("/weather", (req, res) => {
+    res.render("weather", null)
+})
+app.get("/contact", (req, res) => {
+    res.render("contact");
+})
+app.get("*", (req, res) => {
+    res.render("404error", {
+        errorMsg: 'Oops! Page not Found'
+    })
+})
+app.listen(port, () => {
+    console.log(`Listening to the port ${port}`)
+})
